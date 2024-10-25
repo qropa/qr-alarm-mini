69cc40ea43b9262f850be076b884aaddaf421848
qropa <124854119+qropa@users.noreply.github.com>
2024-10-25 20:55:46 +0900

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/qr-alarm-mini/sw.js', { scope: '/qr-alarm-mini/' })})}

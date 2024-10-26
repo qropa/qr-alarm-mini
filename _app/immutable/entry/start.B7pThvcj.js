71d581a058b10e68bd12ec210853ba1b37d2b6d3
qropa <124854119+qropa@users.noreply.github.com>
2024-10-26 18:12:52 +0900

    Updates

---
+++ _app/immutable/entry/start.B7pThvcj.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CTrM9_IP.js";export{t as start};
--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/qr-alarm-mini/sw.js', { scope: '/qr-alarm-mini/' })})}
